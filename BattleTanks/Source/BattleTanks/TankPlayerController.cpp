ddae08f0b786e03cd0d81ea2ab5ffdce7e0d7546
Daniel Esteves <mistermind@live.com>
2017-08-09 23:24:52 -0400

    Udemy Unreal Course - Section 4, Lecture 125

---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "Tank.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	//UE_LOG(LogTemp, Warning, TEXT("Player Controller begin play"));
+	ATank* ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Controller not possessing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
